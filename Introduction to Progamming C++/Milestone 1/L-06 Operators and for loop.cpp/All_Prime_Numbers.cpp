e2fccba1cb7028a486c02da70e8759f1a5b49cf8
Awnish Dubey <dubeyawnish6@gmail.com>
2023-03-10 23:57:06 +0530

    all prime numbers

---
+++ Introduction to Progamming C++/Milestone 1/L-06 Operators and for loop.cpp/All_Prime_Numbers.cpp
@@ -0,0 +1,58 @@
+/*
+Given an integer N, print all the prime numbers that lie in the range 2 to N (both inclusive).
+Print the prime numbers in different lines.
+Input Format :
+Integer N
+Output Format :
+Prime numbers in different lines
+Constraints :
+1 <= N <= 100
+Sample Input 1:
+9
+Sample Output 1:
+2
+3
+5
+7
+Sample Input 2:
+20
+Sample Output 2:
+2
+3
+5
+7
+11
+13
+17
+19
+
+
+*/
+
+#include <iostream>
+using namespace std;
+
+
+bool isPrime(int num){
+	if(num<=1)
+	 return false;
+	 
+	for(int i=2;i*i<=num;i++){
+		if(num%i==0)
+		  return false;
+	}
+	return true;
+}
+
+int main(){
+
+    int n;
+	cin>>n;
+
+	for(int i=2;i<=n;i++){
+		if(isPrime(i)){
+			cout<<i<<endl;
+		}
+	}
+  
+}
