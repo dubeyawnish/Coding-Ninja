1a5f0754948576b13bef33eef6405917bfe145d8
Awnish Dubey <dubeyawnish6@gmail.com>
2023-03-26 22:36:42 +0530

    Arrange Numbers in array

--- Introduction to Progamming C++/Milestone 2/L-08 Array/Array_Sum.cpp
+++ Introduction to Progamming C++/Milestone 2/L-08 Array/Array_Sum.cpp
@@ -1,7 +1,7 @@
 /*
 
 Given an array of length N, you need to find and print the sum of all elements of the array.
-Input Format :
+Input Format 
 Line 1 : An Integer N i.e. size of array
 Line 2 : N integers which are elements of the array, separated by spaces
 Output Format :
@@ -16,6 +16,7 @@ Sample Output :
 
 
 */
+
 
 #include<iostream>
 using namespace std;
